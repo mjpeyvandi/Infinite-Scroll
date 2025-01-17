8ec6236bd25413e0574776e9b3e7998e39dbeccc
Javad <mohammadjp12@gmail.com>
2025-01-17 13:52:01 +0330

    set Loading for fetch users

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -42,76 +42,69 @@ export const Users = () => {
     };
   }, [displayedUsers, page]);
 
-  const loadMoreUsers = () => {
-    const newPage = page + 1;
-    setPage(newPage);
+  const loadMoreUsers = async () => {
+    setLoading(true);
+    const response = await fetch(`https://randomuser.me/api/?results=10&page=${page}`)
+    const newData = await response.json()
 
-    const startIndex = (newPage - 1) * 7;
-    const endIndex = startIndex + 7;
-
-    const newUsers = [];
-    for (let i = startIndex; i < endIndex; i++) {
-      newUsers.push(Users[i % Users.length]);
+    if(newData){
+      setDisplayedUsers((prev)=> [...prev, ...newData.results])
+      setPage((curPage)=> curPage + page)
+      setLoading(false);
     }
-
-    setDisplayedUsers((prevUsers) => [...prevUsers, ...newUsers]);
   };
   return (
     <div className="users">
-      {!Loading ? (
-        displayedUsers?.map((user, key) => (
-          <div className="user__card" key={key}>
-            <div className="user__card__info">
-              <span>{key + 1}</span>
-              <div className="user__card__info__box">
-                <div className="user__card__img">
-                  <img src={user.picture.thumbnail} alt="" className="image" />
-                </div>
-                <div className="user__card__name">
-                  <p className="user__name">
-                    {user.name.title +
-                      "" +
-                      user.name.first +
-                      "" +
-                      user.name.last}
-                  </p>
-                  <span>
-                    {user.login.username} / {user.gender}
-                  </span>
-                </div>
+      {displayedUsers?.map((user, key) => (
+        <div className="user__card" key={key}>
+          <div className="user__card__info">
+            <span>{key + 1}</span>
+            <div className="user__card__info__box">
+              <div className="user__card__img">
+                <img src={user.picture.thumbnail} alt="" className="image" />
               </div>
-            </div>
-            <div className="user__card__details">
-              <div className="user__card__phone">
-                <FaFire className="user__phone__icon" />
-                <span>{user.phone}</span>
+              <div className="user__card__name">
+                <p className="user__name">
+                  {user.name.title + "" + user.name.first + "" + user.name.last}
+                </p>
+                <span>
+                  {user.login.username} / {user.gender}
+                </span>
               </div>
-              <div className="user__card__email">
-                <FaVoicemail />
-                <p>{user.email}</p>
-              </div>
-              <p className="user__card__address">
-                {user.location.city}, {user.location.country},{" "}
-                {user.location.postcode}, {user.location.state}
-              </p>
-            </div>
-            <div className="user__card__country">
-              <img
-                src="https://randomuser.me/api/portraits/thumb/men/78.jpg"
-                alt=""
-                className="user__country__image"
-              />
             </div>
           </div>
-        ))
-      ) : (
+          <div className="user__card__details">
+            <div className="user__card__phone">
+              <FaFire className="user__phone__icon" />
+              <span>{user.phone}</span>
+            </div>
+            <div className="user__card__email">
+              <FaVoicemail />
+              <p>{user.email}</p>
+            </div>
+            <p className="user__card__address">
+              {user.location.city}, {user.location.country},{" "}
+              {user.location.postcode}, {user.location.state}
+            </p>
+          </div>
+          <div className="user__card__country">
+            <img
+              src="https://randomuser.me/api/portraits/thumb/men/78.jpg"
+              alt=""
+              className="user__country__image"
+            />
+          </div>
+        </div>
+      ))}
+
+      {Loading && (
         <DNA
-        visible={true}
-        height="80"
-        width="180"
-        ariaLabel="dna-loading"
-        wrapperStyle={{}}
-        wrapperClass="dna-wrapper"
+          visible={true}
+          height="80"
+          width="180"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
         />
       )}
     </div>
